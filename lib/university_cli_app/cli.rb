a3033c7d3135ebee8dc84def1b52fc9a38929abf
Christine <yutingcxiang@gmail.com>
2018-07-26 17:01:55 -0400

    Fixed class name to be UniversityCliApp

--- lib/university_cli_app/cli.rb
+++ lib/university_cli_app/cli.rb
@@ -1,4 +1,4 @@
-class University::CLI
+class UniversityCliApp::CLI
 
   def call
     list_colleges
@@ -6,30 +6,18 @@ class University::CLI
     close_app
   end
 
-  def list_colleges
-  end
-
-  def list_by_name
-  end
-
-  def list_by_rank
-  end
-
-  def list_by_location
-  end
-
   def menu
     input = nil
-    until input = "5"
-    puts "Welcome to the top 50 Colleges & Universities in America for 2018."
-    puts "Please select an option:"
-    puts "1. View college list"
-    puts "2. Select University by name"
-    puts "3. Select University by rank"
-    puts "4. Select university by location (state)"
-    puts "5. Exit"
-
+    unless input == "5"
+      puts "Welcome to the top 50 Colleges & Universities in America for 2018."
+      puts "Please select an option:"
+      puts "1. View college list"
+      puts "2. Select University by name"
+      puts "3. Select University by rank"
+      puts "4. Select university by location (state)"
+      puts "5. Exit"
     input = gets.strip
+    end
 
     case answer
     when "1"
@@ -41,14 +29,23 @@ class University::CLI
     when "4"
       list_location
     end
+  end
+
+    def list_colleges
+    end
+
+    def list_by_name
+    end
+
+    def list_by_rank
+    end
+
+    def list_by_location
+    end
 
     def close_app
       puts "Thank you for checking out the rankings. Good luck with your selection!"
     end
-
-  end
-
-
 # -> "Would you like additional details? (Y/N)"
 # -> Additional info + website link
 # -> "Would you like to see another college? (Y/N)"
--- lib/university_cli_app/university.rb
+++ lib/university_cli_app/university.rb
@@ -1,24 +1,27 @@
-class University::Top50
+class UniversityCliApp::Top50
   attr_accessor :name, :rank, :location, :url, :acceptance, :ACT, :age, :hhincome, :livingcost, :degreed
+  @@all = []
 
   def initialize
-    self.university_list
+    university_list
   end
 
-  def university_list("https://www.thebestcolleges.org/rankings/top-50/")
-    universities = Nokogiri::HTML(open(url))
+  def university_list
+    universities = Nokogiri::HTML(open("https://www.thebestcolleges.org/rankings/top-50/"))
     university_list = []
-
+    # name: rank-tr js-target is-active
+    # rank: rank-td rank js-trigger
+    university_list.each
     # 1. Princeton University
     # 2. Harvard University
     # 3. Swarthmore College
   end
 
-  def university_page("https://www.thebestcolleges.org/rankings/top-50/")
-    # university = Nokogiri::HTML(open(url))
+  def university_page
+    university = Nokogiri::HTML(open("https://www.thebestcolleges.org/rankings/top-50/"))
     # university = {}
     # Name: Princeton => univerity.css("rankings-list h4.rank_title_link").text
-    # Rank: 1 => university.css(rank-td rank js-trigger ::before")
+    # Rank: 1 => university.css(rank-td rank js-trigger")
     # Location: NJ => univerity.css("rankings-list h4.rank_title_link").text
     # Acceptance Rate: 6%
     # ACT 25th-75th percentile: 32-35
