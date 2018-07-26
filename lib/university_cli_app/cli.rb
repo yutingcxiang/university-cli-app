b564f47dc35f79c2acdee1f63a4260b36a5356d3
Christine <yutingcxiang@gmail.com>
2018-07-25 21:53:23 -0400

    menu options added

--- lib/university_cli_app/cli.rb
+++ lib/university_cli_app/cli.rb
@@ -3,20 +3,52 @@ class University::CLI
   def call
     list_colleges
     menu
+    close_app
   end
 
   def list_colleges
   end
 
-  def menu
+  def list_by_name
   end
-# -"Welcome to the top 50 Colleges & Universities in America for 2018."
-# -"Please select an option:"
-#   1. View college list
-#   2. Select University by name
-#   3. Select University by rank
-#   4. Select university by location (state)
-#   5. Exit
+
+  def list_by_rank
+  end
+
+  def list_by_location
+  end
+
+  def menu
+    input = nil
+    until input = "5"
+    puts "Welcome to the top 50 Colleges & Universities in America for 2018."
+    puts "Please select an option:"
+    puts "1. View college list"
+    puts "2. Select University by name"
+    puts "3. Select University by rank"
+    puts "4. Select university by location (state)"
+    puts "5. Exit"
+
+    input = gets.strip
+
+    case answer
+    when "1"
+      list_colleges
+    when "2"
+      list_by_name
+    when "3"
+      list_by_rank
+    when "4"
+      list_location
+    end
+
+    def close_app
+      puts "Thank you for checking out the rankings. Good luck with your selection!"
+    end
+
+  end
+
+
 # -> "Would you like additional details? (Y/N)"
 # -> Additional info + website link
 # -> "Would you like to see another college? (Y/N)"
