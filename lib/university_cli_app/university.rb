3af944e7fa87de570e29948215e7cf4693a4d676
Christine <yutingcxiang@gmail.com>
2018-07-27 01:58:26 -0400

    Changed University scraper method for table data

--- lib/university_cli_app/university.rb
+++ lib/university_cli_app/university.rb
@@ -1,25 +1,28 @@
 class UniversityCliApp::University
   attr_accessor :name, :rank, :location, :url, :description
 
-  def self.scrape_school_list
+  def self.school_list
     doc = Nokogiri::HTML(open("https://www.thebestcolleges.org/rankings/top-50/"))
-
-    self.scrape_school_list
+    #scrape_school_list
     binding.pry
   end
 
-  def self.school_list
-    #   school_list = []
-    # # universities.css(".rank-tbody") .each do |school|
-    # #   school_list << school = {
-    # #     :name => universities.css(".rank-td title js-trigger h4 a").text,
-    # #     :location => universities.css(".rank-td title js-trigger h4 span").text,
-    # #     :url => universities.css(".rank-td title js-trigger h4 a").attribute("href").value,
-    # #     :description => universities.css(".rank-td stat .stats").text,
-    # #     :rank => universities.css(".rank-td rank js-trigger").text
-    # # }
-    # end
-    # school_list
+  def self.scrape_school_list
+    doc = Nokogiri::HTML(open("https://www.thebestcolleges.org/rankings/top-50/"))
+    school_list = []
+    table = doc.css("table.rankings-list")
+    table.css('tr').each do |tr|
+      tr.css('td').each do |td|
+        school_list << school = {
+          :rank => td.css("rank-td rank js-trigger").text,
+          #:url => td.css("rank-td title js-trigger h4 a").attribute("href").value,
+          :location => td.css("rank-td title js-trigger h4 span").text,
+          :description1 => td.css("rank-td stat copy p").text
+        }
+      end
+    end
+    binding.pry
+    school_list
   end
 
 end
