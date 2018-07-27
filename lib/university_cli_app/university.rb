75022f64d8720cd005d03b249de9c5f3349bfea5
Christine <yutingcxiang@gmail.com>
2018-07-27 00:08:31 -0400

    installed Pry

--- lib/university_cli_app/university.rb
+++ lib/university_cli_app/university.rb
@@ -1,24 +1,25 @@
 class UniversityCliApp::University
   attr_accessor :name, :rank, :location, :url, :description
 
-  def self.school_list
-    self.scrape_university_list
+  def self.scrape_school_list
+    doc = Nokogiri::HTML(open("https://www.thebestcolleges.org/rankings/top-50/"))
+
+    self.scrape_school_list
+    binding.pry
   end
 
-  def self.scrape_university_list
-    universities = Nokogiri::HTML(open("https://www.thebestcolleges.org/rankings/top-50/"))
-    school_list = []
-    # universities.css(".rank-tbody").each do |school|
-    #   school_list << school = {
-    #     :name => universities.css(".rank-td title js-trigger h4 a").text,
-    #     :location => universities.css(".rank-td title js-trigger h4 span").text,
-    #     :url => universities.css(".rank-td title js-trigger h4 a").attribute("href").value,
-    #     :description => universities.css(".rank-td stat .stats").text,
-    #     :rank => universities.css(".rank-td rank js-trigger").text
-    # }
-    end
-    school_list
-    binding.pry
+  def self.school_list
+    #   school_list = []
+    # # universities.css(".rank-tbody") .each do |school|
+    # #   school_list << school = {
+    # #     :name => universities.css(".rank-td title js-trigger h4 a").text,
+    # #     :location => universities.css(".rank-td title js-trigger h4 span").text,
+    # #     :url => universities.css(".rank-td title js-trigger h4 a").attribute("href").value,
+    # #     :description => universities.css(".rank-td stat .stats").text,
+    # #     :rank => universities.css(".rank-td rank js-trigger").text
+    # # }
+    # end
+    # school_list
   end
 
 end
