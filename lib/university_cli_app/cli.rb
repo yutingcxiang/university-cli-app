5841b5ecbec403c642910fdd365b1555349768bb
Christine <yutingcxiang@gmail.com>
2018-07-29 20:18:32 -0400

    CLI list colleges option working

--- lib/university_cli_app/cli.rb
+++ lib/university_cli_app/cli.rb
@@ -2,14 +2,13 @@ class UniversityCliApp::CLI
 
   def call
     menu
-    close_app
   end
 
   def menu
     input = nil
     unless input == "3"
       puts "Welcome to the top 50 Colleges & Universities in America for 2018."
-      puts "Please select an option:"
+      puts "Please select an option and press enter:"
       puts "1. View entire college list"
       puts "2. Select University by rank"
       puts "3. Exit"
@@ -30,19 +29,19 @@ class UniversityCliApp::CLI
 
     def list_colleges
       UniversityCliApp::University.school_list
-      universities = UniversityCliApp::University.school_list
-      universities.each do |school|
-        puts "school.rank school.name"
+      list = UniversityCliApp::University.school_list
+      list.each do |school|
+        puts "#{school[:rank]}. #{school[:name]}"
       end
     end
 
     def list_by_rank
       puts "Please enter a number from 1-50:"
-      answer = gets.strip.to_i
-      universities = UniversityCliApp::University.school_list
-      universities.each do |school|
-        if index + 1 == answer
-        puts "school.rank school.name"
+      answer = gets.strip
+      list = UniversityCliApp::University.school_list
+      list.each do |school|
+        if answer == school.rank
+          puts "#{school[:rank]}. #{school[:name]}"
         end
       end
     end
--- lib/university_cli_app/university.rb
+++ lib/university_cli_app/university.rb
@@ -1,28 +1,28 @@
 # require 'capybara/poltergeist'
 # require 'pry'
 
-options = {
-  js_errors: false,
-}
-
-Capybara.register_driver :poltergeist do |app|
-  Capybara::Poltergeist::Driver.new(app, options)
-end
-
-session = Capybara::Session.new(:poltergeist)
-
-doc = session.visit('https://www.thebestcolleges.org/rankings/top-50/')
-session.find('.js-rankings-expand-all').click
-#puts session.document.title
-
 class UniversityCliApp::University
   attr_accessor :name, :rank, :location, :url, :description
 
-  def school_list
+  def self.school_list
     scrape_school_list
   end
 
   def self.scrape_school_list
+    options = {
+      js_errors: false,
+    }
+
+    Capybara.register_driver :poltergeist do |app|
+      Capybara::Poltergeist::Driver.new(app, options)
+    end
+
+    session = Capybara::Session.new(:poltergeist)
+
+    doc = session.visit('https://www.thebestcolleges.org/rankings/top-50/')
+    session.find('.js-rankings-expand-all').click
+    #puts session.document.title
+
     list = []
     session.all('table.rankings-list tbody tr').each do |item|
       list << {
